ec3ac69bbb09d9a037ba3c02ef03699592f43de4
shwanycom <shwanycom@gmail.com>
2019-05-16 13:39:27 +0900

    5/16AN

--- lib/header_logout_form.php
+++ lib/header_logout_form.php
@@ -79,7 +79,13 @@
         </div>
       </div>
 </header>
-
+<section id="text_category">
+<div id="text_category_text_div">
+<span id="text_category_text_div_span1">Ready-to-use design assets<br>from many independent creators<br></span>
+<span id="text_category_text_div_span2">Graphics, fonts, themes, photos and more, starting at 50MON!<br><br><br></span>
+<span id="text_category_text_div_span3">Get 6 free products and 10% off!</span>
+</div>
+</section>
 <!--템플릿 추가-->
     <section id="categories_section">
       <h2><span class="featured_title">FEATURED CATEGORIES</span></h2>
@@ -90,7 +96,7 @@
             <!--각각 템플릿-->
               <div class="category">
                 <a href="#" data-tracking="Branding Mockups">
-                    <img class="retina" src="./img/animalphoto.png" alt="Branding Mockups">
+                    <img class="retina" src="./img/photo_animal.png" alt="Branding Mockups">
                 </a>
               </div>
           </div>
@@ -98,7 +104,7 @@
             <!--각각 템플릿-->
               <div class="category">
                 <a href="#" data-tracking="Branding Mockups">
-                    <img class="retina" src="./img/foodphoto.png" alt="Branding Mockups">
+                    <img class="retina" src="./img/photo_food.png" alt="Branding Mockups">
                 </a>
               </div>
           </div>
@@ -106,7 +112,7 @@
             <!--각각 템플릿-->
               <div class="category">
                 <a href="#" data-tracking="Branding Mockups">
-                    <img class="retina" src="./img/symbolfont.png" alt="Branding Mockups">
+                    <img class="retina" src="./img/font_symbol.png" alt="Branding Mockups">
                 </a>
               </div>
           </div>
@@ -114,7 +120,7 @@
             <!--각각 템플릿-->
               <div class="category">
                 <a href="#" data-tracking="Branding Mockups">
-                    <img class="retina" src="./img/brushfont.png" alt="Branding Mockups">
+                    <img class="retina" src="./img/font_brush.png" alt="Branding Mockups">
                 </a>
               </div>
           </div>
@@ -122,7 +128,7 @@
             <!--각각 템플릿-->
               <div class="category">
                 <a href="#" data-tracking="Branding Mockups">
-                    <img class="retina" src="./img/texturegraphic.png" alt="Branding Mockups">
+                    <img class="retina" src="./img/graphic_texture.png" alt="Branding Mockups">
                 </a>
               </div>
           </div>
@@ -130,7 +136,7 @@
             <!--각각 템플릿-->
               <div class="category">
                 <a href="#" data-tracking="Branding Mockups">
-                    <img class="retina" src="./img/patterngraphic.png" alt="Branding Mockups">
+                    <img class="retina" src="./img/graphic_pattern.png" alt="Branding Mockups">
                 </a>
               </div>
           </div>
