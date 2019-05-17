c9015f2088ca3f469b4cd1d9368cc5ff680e63bf
shwanycom <shwanycom@gmail.com>
2019-05-17 17:27:12 +0900

    17:27

---
+++ lib/section_categories_section_in_folder.php
@@ -0,0 +1,41 @@
+    <section id="categories_section">
+      <h2><span class="featured_title">FEATURED CATEGORIES</span></h2>
+      <div class="container">
+      <!--각각 템플릿-->
+        <div class="category">
+          <a href="#" data-tracking="Branding Mockups">
+              <img class="retina" src="../img/photo_animal.png" alt="Branding Mockups">
+          </a>
+        </div>
+      <!--각각 템플릿-->
+        <div class="category">
+          <a href="#" data-tracking="Branding Mockups">
+              <img class="retina" src="../img/photo_food.png" alt="Branding Mockups">
+          </a>
+        </div>
+      <!--각각 템플릿-->
+        <div class="category">
+          <a href="#" data-tracking="Branding Mockups">
+              <img class="retina" src="../img/font_symbol.png" alt="Branding Mockups">
+          </a>
+        </div>
+      <!--각각 템플릿-->
+        <div class="category">
+          <a href="#" data-tracking="Branding Mockups">
+              <img class="retina" src="../img/font_brush.png" alt="Branding Mockups">
+          </a>
+        </div>
+      <!--각각 템플릿-->
+        <div class="category">
+          <a href="#" data-tracking="Branding Mockups">
+              <img class="retina" src="../img/graphic_texture.png" alt="Branding Mockups">
+          </a>
+        </div>
+      <!--각각 템플릿-->
+        <div class="category">
+          <a href="#" data-tracking="Branding Mockups">
+              <img class="retina" src="../img/graphic_pattern.png" alt="Branding Mockups">
+          </a>
+        </div>
+      </div>
+    </section>
