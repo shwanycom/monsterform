6a39b13bc33a5a9a873c2cf285f7404904bf5a67
likeny <likeny@naver.com>
2019-05-17 16:48:47 +0900

    16:48 - khy

--- discussion/list.php
+++ discussion/list.php
@@ -99,96 +99,9 @@ if(isset($_GET["mode"]) && $_GET["mode"] == "search"){
      <title></title>
    </head>
    <body>
-     <header id="header">
-       <div id="header_logout_form_div1">
-           <div id="header_logout_form_div1_1">
-             <ul id="header_logout_form_div1_1_ul">
-               <li class="header_logout_form_div1_1_ul_li"><a href="#">Get Free Goods</a></li>
-               <li class="header_logout_form_div1_1_ul_li"><a href="#">Become a Partner</a></li>
-               <li class="header_logout_form_div1_1_ul_li"><a href="#">Discussions</a></li>
-             </ul>
-           </div>
-             <div id="header_logout_form_div1_2">
-               <?php
-               ?><ul id="header_logout_form_div1_2_ul">
-                 <li class="header_logout_form_div1_2_ul_li"><button type="button" name="header_logout_form_div1_ul1_li2_button2" id="myBtn2">Sign in</button></li>
-                 <li class="header_logout_form_div1_2_ul_li">&nbsp;or&nbsp;</li>
-                 <li class="header_logout_form_div1_2_ul_li"><button type="button" name="header_logout_form_div1_ul1_li2_button1" id="myBtn">Join Now</button></li>
-                 <li class="header_logout_form_div1_2_ul_li"><button type="button" name="header_logout_form_div1_ul1_li2_button1" id="myBtn3">Sign out</button></li>
-               </ul>
-             </div>
-           </div>
-
-           <div id="header_logout_form_div2">
-             <div id="header_logout_form_div2_1">
-               <img id="logo" src="../img/logo.png" alt="">
-             </div>
-             <div id="header_logout_form_div2_2">
-               <ul>
-                 <li class="header_logout_form_div2_ul1_li">
-                   <a class="header_logout_form_div2_ul1_li_a" href="#">Photos</a>
-                   <ul class="submenu">
-                     <li><a href="#" class="subheader_logout_form_div2_ul1_li_a">Abstract</a></li>
-                     <li><a href="#" class="subheader_logout_form_div2_ul1_li_a">Animals</a></li>
-                     <li><a href="#" class="subheader_logout_form_div2_ul1_li_a">Architecture</a></li>
-                     <li><a href="#" class="subheader_logout_form_div2_ul1_li_a">Arts &Entertainment</a></li>
-                     <li><a href="#" class="subheader_logout_form_div2_ul1_li_a">Beauty & Fashion</a></li>
-                     <li><a href="#" class="subheader_logout_form_div2_ul1_li_a">Business</a></li>
-                     <li><a href="#" class="subheader_logout_form_div2_ul1_li_a">Education</a></li>
-                     <li><a href="#" class="subheader_logout_form_div2_ul1_li_a">Food & Drink</a></li>
-                     <li><a href="#" class="subheader_logout_form_div2_ul1_li_a">Health</a></li>
-                     <li><a href="#" class="subheader_logout_form_div2_ul1_li_a">Holidays</a></li>
-                     <li><a href="#" class="subheader_logout_form_div2_ul1_li_a">Industrial</a></li>
-                     <li><a href="#" class="subheader_logout_form_div2_ul1_li_a">Nature</a></li>
-                     <li><a href="#" class="subheader_logout_form_div2_ul1_li_a">People</a></li>
-                     <li><a href="#" class="subheader_logout_form_div2_ul1_li_a">Sports</a></li>
-                     <li><a href="#" class="subheader_logout_form_div2_ul1_li_a">Technology</a></li>
-                     <li><a href="#" class="subheader_logout_form_div2_ul1_li_a">Transportation</a></li>
-                   </ul>
-                 </li>
-
-                 <li class="header_logout_form_div2_ul1_li"> <a class="header_logout_form_div2_ul1_li_a" href="#">Graphics</a>
-                   <ul class="submenu">
-                     <li><a href="#" class="subheader_logout_form_div2_ul1_li_a">Icons</a></li>
-                     <li><a href="#" class="subheader_logout_form_div2_ul1_li_a">Illustrations</a></li>
-                     <li><a href="#" class="subheader_logout_form_div2_ul1_li_a">Web Elements</a></li>
-                     <li><a href="#" class="subheader_logout_form_div2_ul1_li_a">Objects</a></li>
-                     <li><a href="#" class="subheader_logout_form_div2_ul1_li_a">Patterns</a></li>
-                     <li><a href="#" class="subheader_logout_form_div2_ul1_li_a">Textures</a></li>
-                   </ul>
-                 </li>
-                 <li class="header_logout_form_div2_ul1_li"> <a class="header_logout_form_div2_ul1_li_a" href="#">Font</a>
-                   <ul class="submenu">
-                     <li><a href="#" class="subheader_logout_form_div2_ul1_li_a">Blackletter</a></li>
-                     <li><a href="#" class="subheader_logout_form_div2_ul1_li_a">Display</a></li>
-                     <li><a href="#" class="subheader_logout_form_div2_ul1_li_a">Non Western</a></li>
-                     <li><a href="#" class="subheader_logout_form_div2_ul1_li_a">Sans Serif</a></li>
-                     <li><a href="#" class="subheader_logout_form_div2_ul1_li_a">Script</a></li>
-                     <li><a href="#" class="subheader_logout_form_div2_ul1_li_a">Serif</a></li>
-                     <li><a href="#" class="subheader_logout_form_div2_ul1_li_a">Slab Serif</a></li>
-                     <li><a href="#" class="subheader_logout_form_div2_ul1_li_a">Symbols</a></li>
-                   </ul>
-                 </li>
-               </ul>
-             </div>
-
-             <div class="header_logout_form_div5">
-                 <label id="search_label"><img src="../img/zoom.png" id="search_img" style="width:15px; height:15px; padding-top:2px; padding:0;"> <input type="text" id="search_text" placeholder="Search"> </label>
-               <div id="header_logout_form_div2_3">
-                   <a href="#" id="header_logout_form_div2_3_a" onmouseover="mouse_over()" onmouseout="mouse_out()">
-                     <span id="header_logout_form_div2_3_span">All</span>
-                     <img id="change_img" src="../img/down.png" style="width:15px; height:15px;"/>&nbsp;&nbsp;&nbsp;
-                   </a>
-                 <ul id="header_logout_form_div2_3_ul">
-                   <li class="header_logout_form_div2_3_ul_li" id="All" onclick="select('All')" value="allcategories"><a href="#">All&nbspcategories</a></li>
-                   <li class="header_logout_form_div2_3_ul_li" id="photos" onclick="select('Photos')" value="Photos"><a href="#">Photos</a></li>
-                   <li class="header_logout_form_div2_3_ul_li" id="Graphics" onclick="select('Graphics')" value="Graphics"><a href="#">Graphics</a></li>
-                   <li class="header_logout_form_div2_3_ul_li" id="Fonts" onclick="select('Fonts')" value="Fonts"><a href="#">Fonts</a></li>
-                 </ul>
-               </div>
-             </div>
-           </div>
-     </header>
+     <?php
+     include "../lib/header_logout_form_in_folder.php";
+      ?>
 
      <!--============================================================================== -->
      <br><br><br><br><br>
@@ -230,17 +143,15 @@ if(isset($_GET["mode"]) && $_GET["mode"] == "search"){
                   $content = str_replace("\n", "<br>", $content);
                   $content= str_replace(" ", "&nbsp;", $content);
                   $date = substr($row['regist_day'], 0, 10);
-
                 ?>
               <div id="list_item">
                 <div id="general_img_div">
-                  작성자<br>사진
+                  제목<br>작성자
                 </div>
                 <ul id="general_ul">
                   <li id="list_item2"><a href="./view.php?num=<?=$num?>&page=<?=$page?>"><?=$subject?></a></li>
                   <br>
-                  <li id="list_item3"><?=$id?></li>
-                  <li id="list_item4">(<?=$email?>)</li>
+                  <li id="list_item3"><a href="../members/collections/<?=$email?>"><?=$id."($email)"?></a></li>
                   <li id="list_item5"><?=$date?></li>
                 </ul>
               </div> <!-- end of list_item -->
@@ -301,13 +212,12 @@ if(isset($_GET["mode"]) && $_GET["mode"] == "search"){
                   ?>
                 <div id="list_item">
                   <div id="general_img_div">
-                    작성자<br>사진
+                    제목<br>작성자
                   </div>
                   <ul id="general_ul">
                     <li id="list_item2"><a href="./view.php?num=<?=$num?>&page=<?=$page?>"><?=$subject?></a></li>
                     <br>
-                    <li id="list_item3"><?=$id?></li>
-                    <li id="list_item4">(<?=$email?>)</li>
+                    <li id="list_item3"><a href="../members/collections/<?=$email?>"><?=$id."($email)"?></a></li>
                     <li id="list_item5"><?=$date?></li>
                   </ul>
                 </div> <!-- end of list_item -->
@@ -357,13 +267,12 @@ if(isset($_GET["mode"]) && $_GET["mode"] == "search"){
                   ?>
                 <div id="list_item">
                   <div id="general_img_div">
-                    작성자<br>사진
+                    제목<br>작성자
                   </div>
                   <ul id="general_ul">
                     <li id="list_item2"><a href="./view.php?num=<?=$num?>&page=<?=$page?>"><?=$subject?></a></li>
                     <br>
-                    <li id="list_item3"><?=$id?></li>
-                    <li id="list_item4">(<?=$email?>)</li>
+                    <li id="list_item3"><a href="../members/collections/<?=$email?>"><?=$id."($email)"?></a></li>
                     <li id="list_item5"><?=$date?></li>
                   </ul>
                 </div> <!-- end of list_item -->
@@ -412,13 +321,12 @@ if(isset($_GET["mode"]) && $_GET["mode"] == "search"){
                   ?>
                 <div id="list_item">
                   <div id="general_img_div">
-                    작성자<br>사진
+                    제목<br>작성자
                   </div>
                   <ul id="general_ul">
                     <li id="list_item2"><a href="./view.php?num=<?=$num?>&page=<?=$page?>"><?=$subject?></a></li>
                     <br>
-                    <li id="list_item3"><?=$id?></li>
-                    <li id="list_item4">(<?=$email?>)</li>
+                    <li id="list_item3"><a href="../members/collections/<?=$email?>"><?=$id."($email)"?></a></li>
                     <li id="list_item5"><?=$date?></li>
                   </ul>
                 </div> <!-- end of list_item -->
@@ -468,13 +376,12 @@ if(isset($_GET["mode"]) && $_GET["mode"] == "search"){
                   ?>
                 <div id="list_item">
                   <div id="general_img_div">
-                    작성자<br>사진
+                    제목<br>작성자
                   </div>
                   <ul id="general_ul">
                     <li id="list_item2"><a href="./view.php?num=<?=$num?>&page=<?=$page?>"><?=$subject?></a></li>
                     <br>
-                    <li id="list_item3"><?=$id?></li>
-                    <li id="list_item4">(<?=$email?>)</li>
+                    <li id="list_item3"><a href="../members/collections/<?=$email?>"><?=$id."($email)"?></a></li>
                     <li id="list_item5"><?=$date?></li>
                   </ul>
                 </div> <!-- end of list_item -->
@@ -509,64 +416,8 @@ if(isset($_GET["mode"]) && $_GET["mode"] == "search"){
         <div class="clear"></div>
         <br><br>
         <!--===============================섹션영역=================================== -->
-        <footer class="footer_footer">
-          <div id="footer_main">
-            <div id="footer_main_margin_left_right">
-              <div id="footer_div1">
-                <div id="footer_div1_div1">
-                  <a href="#"><img id="footer_logo"src="../img/logo.png"></a><br><br>
-                    MonsterForm is the world’s marketplace for
-                    <br>design. Bring your creative projects to life with
-                    <br>ready-to-use design assets from independent
-                    <br>creators around the world.
-                </div>
-                <div id="footer_div1_div2">
-                  <div class="footer_div1_div2_div">
-                    Earn<br><br><br>
-                    <ul>
-                      <li><a href="">Open a Shop</a></li>
-                      <li><a href="">Become a Partner</a></li>
-                    </ul>
-                  </div>
-                  <div class="footer_div1_div2_div">
-                      Resources<br><br><br>
-                      <ul>
-                        <li><a href="">Discussions</a></li>
-                        <li><a href="">Products</a></li>
-                        <li><a href="">Collections</a></li>
-                        <li><a href="">Help Center</a></li>
-                      </ul>
-                    </div>
-                    <div class="footer_div1_div2_div">
-                      The Goods<br><br><br>
-                      <ul>
-                        <li><a href="">Free Goods</a></li>
-                        <li><a href="">Purchase Credits</a></li>
-                        <li><a href="">Gift Cards</a></li>
-                      </ul>
-                    </div>
-                    <div class="footer_div1_div2_div" id="footer_div1_div2_div5_id">
-                      Company<br><br><br>
-                      <ul>
-                        <li><a href="">About</a></li>
-                        <li><a href="">Brand</a></li>
-                        <li><a href="">Terms</a></li>
-                        <li><a href="">Cookie Policy</a></li>
-                        <li><a href="">Privacy Policy</a></li>
-                      </ul>
-                    </div>
-                </div>
-              </div>
-              <div id="footer_div2">
-                <ul class="stats">
-                  <li><?php echo "3,345,959"; ?>&nbsp;Products</li>
-                  <li><?php echo "5,922,327"; ?>&nbsp;Members</li>
-                  <li><?php echo "29,318"; ?>&nbsp;Shops</li>
-                  <li>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&copy; 2019 Monster Form. All rights reserved.</li>
-                </ul>
-              </div>
-            </div>
-          </div>
-        </footer>
+        <?php
+          include "../lib/footer_in_folder.php";
+         ?>
    </body>
  </html>
