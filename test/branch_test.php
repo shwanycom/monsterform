8081642412dc48145ea2680b4e7297045a0ff8ef
shwanycom <shwanycom@gmail.com>
2019-05-17 17:42:00 +0900

    17:41

--- test/branch_test.php
+++ test/branch_test.php
@@ -1,18 +1,39 @@
-brunch test입니다.
-
-정승환이 작업중입니다.
-정승환이 작업중입니다2.
-
-나 이동현인데 정승환 나와바라
-<<<<<<< HEAD
-<<<<<<< HEAD
-뻐킹브랜치
-=======
->>>>>>> 4510c1f919f6046f5d6944aa8340e86317e54f01
->>>>>>> 6faf7d9e236ef345cf06334ba67ecaad2a6c54d5
-=======
-
-스밤바라밤밤
-
-풀을 안댕기고 푸쉬를 한다이크
->>>>>>> bd8ac443684ce36e4bdbe5eaa26473052482fd8d
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+    <style >
+    .a{
+      height: 500px;
+      width: 700px;
+      border:1px solid red;
+    }
+    .b{
+      height: 200px;
+      width: 300px;
+      border:1px solid red;
+      display: inline-block;
+      vertical-align: middle;
+    }
+    .c{
+      height: 200px;
+      width: 300px;
+      border:1px solid red;
+      display: inline-block;
+      vertical-align: middle;
+    }
+    </style>
+  </head>
+  <body>
+    <div class="a">
+      <div class="b">
+        asdasd
+      </div>
+      <div class="c">
+        asdas<br>
+        qwdqdqwd
+      </div>
+    </div>
+  </body>
+</html>
