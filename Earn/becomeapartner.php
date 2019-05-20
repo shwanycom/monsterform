55c2863c4029d2190449fc6745175539994a70eb
shwanycom <shwanycom@gmail.com>
2019-05-20 20:09:29 +0900

    20:09JSH

--- Earn/becomeapartner.php
+++ Earn/becomeapartner.php
@@ -7,34 +7,16 @@
     <meta charset="utf-8">
     <title></title>
     <link rel="stylesheet" href="../css/common.css?ver=1">
+    <link rel="stylesheet" href="../css/becomeapartner.css">
+    <link rel="stylesheet" href="../css/footer.css">
     <style media="screen">
-    #become_section{
-      text-align: center;
-    }
-    #become_section1 button{
-      width:200px;
-      height:40px;
-      background-color: rgba(139,167,83,118);
-      color:rgb(245,245,244,230);
-      border:none;
-      border-radius: 5px;
-    }
-    #become_section1 button:hover{
-      width:200px;
-      height:40px;
-      background-color: rgba(151,177,98,129);
-      color:rgb(245,245,244,230);
-      border: none;
-      border-radius: 5px;
-    }
-
     </style>
   </head>
   <body>
     <?php include "../lib/header_in_folder.php"; ?>
     <div id="become_section">
       <div id="become_section1">
-        <img src="" alt="">
+        <img src="../img/become_section1.png" alt="">
         <h1>Apply to become an affilate partner</h1>
         <p>Earn up to 10% every purchase for an entire tear from all new customers you refer.</p>
         <button type="button" name="button">Become a Partner</button>
@@ -43,33 +25,34 @@
       <div id="become_section2">
         <h1>Spread The Word and Earn</h1>
         <p>Earn 10% on all referred Monster Form customer purchases for a full year.</p>
-        <img src="" alt="sec2">
+        <img src="" alt="">
       </div>
 
       <div id="become_section3">
         <h1>Referrals Last Up to an Entire Year</h1>
         <p>Earn money time a customer or subscriber you've refferred makes a purchase in their first year.</p>
-        <img src="" alt="sec3">
+        <img src="" alt="">
       </div>
 
       <div id="become_section4">
         <h1>Build Your Empire</h1>
         <p>Each new customer you refer grows your partner empire, so you can earn money while you sleep, just for <b>telling about Monster Form</b> </p>
-        <img src="" alt="sec4">
+        <img src="" alt="">
       </div>
 
       <div id="become_section5">
         <h1>Support the Creators</h1>
         <p>Promoting products on Monster Form enables independent designers and creators around the world to <b>spend more time doing what they love.</b></p>
-        <img src="" alt="sec5">
+        <img src="" alt="">
       </div>
 
       <div id="become_section6">
          <p>Apply today and start spreading the word.</p>
-        <input type="button" name="" value="Become a Partner">
+        <button type="button" name="button">Become a Partner</button>
       </div>
     </div>
 
-
+    <?php include "../lib/footer_in_folder.php"; ?>
+    <?php include "../khy_modal/login_modal.php"; ?>
   </body>
 </html>
