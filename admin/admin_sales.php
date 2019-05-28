4460fb52699305a5e029d85aa1e70ae113b94329
shwanycom <shwanycom@gmail.com>
2019-05-28 15:41:12 +0900

    15:41JSH

--- admin/admin_sales.php
+++ admin/admin_sales.php
@@ -139,18 +139,20 @@ include_once $_SERVER["DOCUMENT_ROOT"]."./monsterform/lib/db_connector.php";
     $sql_photos_total = "select sum(report_price) as sum_ph from `report` as r  inner join `products` as p on p.num=r.product_num where p.big_data='photos' and r.report_regist_day like '$year-$month%';";
     $sql_graphics_total = "select sum(report_price) as sum_gr from `report` as r  inner join `products` as p on p.num=r.product_num where p.big_data='graphics' and r.report_regist_day like '$year-$month%';";
     $sql_fonts_total = "select sum(report_price) as sum_fo from `report` as r  inner join `products` as p on p.num=r.product_num where p.big_data='fonts' and like r.report_regist_day '$year-$month%';;";
+
+    $result_photo = mysqli_query($conn, $sql_photos_total);
+    $row_ph=mysqli_fetch_array($result_photo);
+    $sum_ph = $row_ph['sum_ph'];
+
+    $result_graphics = mysqli_query($conn, $sql_graphics_total);
+    $row_gr=mysqli_fetch_array($result_graphics);
+    $sum_gr=$row_gr['sum_gr'];
+
+    $result_fonts = mysqli_query($conn, $sql_fonts_total);
+    $row_fo=mysqli_fetch_array($result_fonts);
+    $sum_fo=$row_fo['sum_fo'];
   }
-  $result_photo = mysqli_query($conn, $sql_photos_total);
-  $row_ph=mysqli_fetch_array($result_photo);
-  $sum_ph = $row_ph['sum_ph'];
 
-  $result_graphics = mysqli_query($conn, $sql_graphics_total);
-  $row_gr=mysqli_fetch_array($result_graphics);
-  $sum_gr=$row_gr['sum_gr'];
-
-  $result_fonts = mysqli_query($conn, $sql_fonts_total);
-  $row_fo=mysqli_fetch_array($result_fonts);
-  $sum_fo=$row_fo['sum_fo'];
 ?>
 <html>
   <head>
