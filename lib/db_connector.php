50f72acf48e34c386ada1cd743b45ff04b73002b
merong2969 <merong2969@gmail.com>
2019-06-02 14:21:09 +0900

    14:21 lee

--- lib/db_connector.php
+++ lib/db_connector.php
@@ -1,9 +1,12 @@
 <?php
 date_default_timezone_set("Asia/Seoul");
 
-$servername = "192.168.0.174";
-$username = "monsterform";
-$password = "monster1!";
+// $servername = "192.168.0.174";
+// $username = "monsterform";
+// $password = "monster1!";
+$servername = "localhost";
+$username = "root";
+$password = "123456";
 
 
 
