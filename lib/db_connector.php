05a58e2bd2e5847f7810586a8fb4683e66f04f03
likeny <likeny@naver.com>
2019-06-02 14:15:30 +0900

    14:15 - khy

--- lib/db_connector.php
+++ lib/db_connector.php
@@ -1,10 +1,13 @@
 <?php
 date_default_timezone_set("Asia/Seoul");
 
-$servername = "192.168.0.174";
-$username = "monsterform";
-$password = "monster1!";
+// $servername = "192.168.0.174";
+// $username = "monsterform";
+// $password = "monster1!";
 
+$servername = "localhost";
+$username = "root";
+$password = "123456";
 
 
 $dbflag = "NO";
