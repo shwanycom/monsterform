859226545c421b89a180078cb08697a55e18ea08
shwanycom <shwanycom@gmail.com>
2019-06-03 09:34:28 +0900

    jsh0934

--- lib/section_text_category.php
+++ lib/section_text_category.php
@@ -2,6 +2,6 @@
 <div id="text_category_text_div"><br><br><br>
 <span id="text_category_text_div_span1">Ready-to-use design assets<br>from many independent creators<br></span>
 <span id="text_category_text_div_span2">Graphics, fonts, photos starting at 50MON!<br><br><br></span>
-<span id="text_category_text_div_span3">Get 6 free products and 10% off!</span><a href="./point/point_main.php"><button type="button" name="button" id="continue">Continue →</button></a>
+<span id="text_category_text_div_span3">Get 6 free products and 10% off!</span><a href="./goods/freegoods.php"><button type="button" name="button" id="continue">Continue →</button></a>
 </div>
 </section>
